667a978e27b830cf2363edc29cd4d824dc93ac41
QuintMin122903 <QuintMin122903@users.noreply.github.com>
2026-06-04 17:00:34 +0800

    Add build scripts and solution files

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# get path of current script: https://stackoverflow.com/a/39340259/207661
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$SCRIPT_DIR" >/dev/null
+
+set -e
+set +x
+
+rm -rf build
+rm -rf cmake/output
+
+popd >/dev/null
